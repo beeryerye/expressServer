2608f5910d2ae3d7df9788cb1e297b655e0f7535
beeryerye <bjkropff@gmail.com>
2019-08-08 11:58:03 -0700

    Add final view details

--- app.js
+++ app.js
@@ -24,5 +24,32 @@ app.get('/contact', function(req, res){
 	res.render('contact');
 });
 
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport ({
+		service: 'Gmail',
+		auth: {
+			user: 'me@g.mail',
+			pass: ''
+		}
+	});
+	var mailOptions = {
+		from: 'Dude Duderson <me@g.mail',
+		to : 'other@g.mail',
+		subject: 'Submission',
+		text: 'You have submission with details: Name: '+req.body.name+', Email: '+req.body.email+', Message: '+req.body.message,
+		html: 'You have submission with details:<ui><li> Name: '+req.body.name+'</li><li> Email: '+req.body.email+' </li><li>Message: '+req.body.message+'</li></ui><p>'
+	}
+
+  transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
+
 app.listen(3000);
 console.log('Server is running on port 3000...');
